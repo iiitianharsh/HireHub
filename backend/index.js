5c8830d225c0af23deb425d96be76ccbc2354a83
Harsh Jaiswal <2003harshjais@gmail.com>
2024-08-01 01:22:01 +0530

    Completed Backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 import express from "express";
- import cookieParser from "cookie-parser";
- import cors from "cors";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
- import userRoute from "./routes/user.route.js";
- import companyRoute from "./routes/company.route.js";
-// import jobRoute from "./routes/job.route.js";
-// import applicationRoute from "./routes/application.route.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
 
@@ -14,27 +14,27 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',  // for frontend 
-    credentials:true
+    origin: 'http://localhost:5173',  // for frontend 
+    credentials: true
 }
 
- app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
- const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 
 // api's
 app.use("/api/v1/user", userRoute);
- app.use("/api/v1/company", companyRoute);
-// app.use("/api/v1/job", jobRoute);
-// app.use("/api/v1/application", applicationRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
 })
