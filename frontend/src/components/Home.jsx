8c831c49f3ad898e13ec3bf2a6fa79f4602c8bb5
Harsh Jaiswal <2003harshjais@gmail.com>
2024-08-03 16:31:01 +0530

    Added New Pages

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -9,14 +9,14 @@ import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-//   useGetAllJobs();
-//   const { user } = useSelector(store => store.auth);
-//   const navigate = useNavigate();
-//   useEffect(() => {
-//     if (user?.role === 'recruiter') {
-//       navigate("/admin/companies");
-//     }
-//   }, []);
+  useGetAllJobs();
+  const { user } = useSelector(store => store.auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user?.role === 'recruiter') {
+      navigate("/admin/companies");
+    }
+  }, []);
   return (
     <div>
       <Navbar />
