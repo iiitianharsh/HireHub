71e8715250d8b1b3b7cd9a0e7da516b5812e5fe2
Harsh Jaiswal <>
2024-10-31 10:50:38 +0530

    Added vercel

--- frontend/src/hooks/useGetCompanyById.jsx
+++ frontend/src/hooks/useGetCompanyById.jsx
@@ -1,18 +1,17 @@
 import { setSingleCompany } from '@/redux/companySlice'
-import { setAllJobs } from '@/redux/jobSlice'
-import { COMPANY_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant'
+import { COMPANY_API_END_POINT } from '@/utils/constant'
 import axios from 'axios'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
 const useGetCompanyById = (companyId) => {
     const dispatch = useDispatch();
-    useEffect(()=>{
+    useEffect(() => {
         const fetchSingleCompany = async () => {
             try {
-                const res = await axios.get(`${COMPANY_API_END_POINT}/get/${companyId}`,{withCredentials:true});
+                const res = await axios.get(`${COMPANY_API_END_POINT}/get/${companyId}`, { withCredentials: true });
                 console.log(res.data.company);
-                if(res.data.success){
+                if (res.data.success) {
                     dispatch(setSingleCompany(res.data.company));
                 }
             } catch (error) {
@@ -20,7 +19,7 @@ const useGetCompanyById = (companyId) => {
             }
         }
         fetchSingleCompany();
-    },[companyId, dispatch])
+    }, [companyId, dispatch])
 }
 
 export default useGetCompanyById
