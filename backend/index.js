7274a01f9e0d6dddf62bc758fe7701505f1d8396
Harsh Jaiswal <>
2024-11-08 00:03:23 +0530

    Revert "Updated databse"
    This reverts commit 99f8bd6daf2f672436312924c34af5f218f882fa.

--- backend/index.js
+++ backend/index.js
@@ -8,32 +8,29 @@ import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-dotenv.config({});
+dotenv.config();
 
 const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+    origin: 'http://localhost:5173',  // for frontend 
+    credentials: true
+};
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
-
-// api's
+// api routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+// Connect to the database when starting the server
+connectDB();
 
- 
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})  
+// Export the app as a module for Vercel
+export default app;
